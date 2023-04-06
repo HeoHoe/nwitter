d02e8088a7e4c31bf52502044ee35a12205e33d3
SAMSUNG_DEV\developer <silvercolor@hanmail.net>
2023-04-06 19:43:52 +0900

    수정

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@
 import firebase from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDeMPy_Ak-7xjE7N-5SRm4bzcYKiIams_s",
-    authDomain: "nwitter-f2d51.firebaseapp.com",
-    projectId: "nwitter-f2d51",
-    storageBucket: "nwitter-f2d51.appspot.com",
-    messagingSenderId: "730940715238",
-    appId: "1:730940715238:web:8f85eafa930e038ba88b27"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 export default firebase.initializeApp(firebaseConfig);
